4216823690d1b51f0b9591f0add7163b98a49241
Kirill Mehtiev <Kirill Mehtiev>
2017-02-28 15:29:34 +0200

    Play with threads

---
+++ code-examples/Threads/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace Threads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main thread: starting a dedicated thread" +
+                "to do asynchronous operation");
+            var thread = new Thread(ComputeBoundOp);
+            thread.Start(5);
+
+            Console.WriteLine("Main thread: Doing other work here...");
+            Thread.Sleep(10000);
+
+            thread.Join();
+            Console.WriteLine("Hit <Enter> to exit the program...");
+            Console.ReadKey();
+        }
+
+        private static void ComputeBoundOp(Object state)
+        {
+            Console.WriteLine("In ComputeBondOp: state={0}", state);
+            //Thread.Sleep(1000);
+        }
+    }
+}
